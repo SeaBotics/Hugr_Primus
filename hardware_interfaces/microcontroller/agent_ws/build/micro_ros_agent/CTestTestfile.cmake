a952df5c2a2785c4d87361ef8aa6b7cd1fb0b22a
Isak <dagisaks@gmail.com>
2025-09-27 23:54:49 +0200

    add workspace for MicroROS, and ROSAgent, for the NUCLEO STM32 L4R5ZI

---
+++ hardware_interfaces/microcontroller/agent_ws/build/micro_ros_agent/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/isak/seabotics_ws/Hugr_Primus/hardware_interfaces/microcontroller/agent_ws/src/uros/micro-ROS-Agent/micro_ros_agent
+# Build directory: /home/isak/seabotics_ws/Hugr_Primus/hardware_interfaces/microcontroller/agent_ws/build/micro_ros_agent
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ hardware_interfaces/microcontroller/agent_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/isak/seabotics_ws/Hugr_Primus/hardware_interfaces/microcontroller/agent_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
