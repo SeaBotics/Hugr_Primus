e691c61d8d06b6c6d83f86d4f2e4f7fdc3b6710d
Isak <dagisaks@gmail.com>
2025-10-07 21:13:22 +0200

    Reverter til tidligere versjon med .gitignore-commit og fjerner senere endringer

--- hardware_interfaces/microcontroller/agent_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/isak/seabotics_ws/Hugr_Primus/hardware_interfaces/microcontroller/agent_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
