0e2e7c2a1ba20dbab6d8b52e3f31750af8062673
Carlos Mauricio Leal Andersen <carlos.mauricio.leal@outlook.com>
2026-03-09 17:21:24 +0100

    remove ignored files. Add ros2 package for downloading and building OpenCV library and depthAI drivers

---
+++ seabotics_ws/src/external_drivers/luxonis_ros2_driver/depthai_driver_loader/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'depthai_driver_loader'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='carlos',
+    maintainer_email='carlos.mauricio.leal@outlook.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        	'download = depthai_driver_loader.download_depthai:main'
+	],
+    },
+)
