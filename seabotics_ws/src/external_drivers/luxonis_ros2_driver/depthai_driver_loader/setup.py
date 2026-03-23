52e53c433573a2166cd0e97201f79960bf06feeb
Carlos Mauricio Leal Andersen <carlos.mauricio.leal@outlook.com>
2026-03-23 17:12:59 +0100

    Update depthAI loader to include depthAI SDK for python

--- seabotics_ws/src/external_drivers/luxonis_ros2_driver/depthai_driver_loader/setup.py
+++ seabotics_ws/src/external_drivers/luxonis_ros2_driver/depthai_driver_loader/setup.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from setuptools import find_packages, setup
 
 package_name = 'depthai_driver_loader'
@@ -28,3 +29,39 @@ setup(
 	],
     },
 )
+=======
+
+from setuptools import find_packages, setup
+
+package_name = 'depthai_driver_loader'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+	'setuptools',
+	'depthai>=2.22.0',
+	],
+    zip_safe=True,
+    maintainer='carlos',
+    maintainer_email='carlos.mauricio.leal@outlook.com',
+    description='Utility package that downloads, builds and loads Luxonis DepthAI ROS-2 drivers',
+    license='TODO',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        	'download = depthai_driver_loader.download_depthai:main'
+	],
+    },
+)
+>>>>>>> Stashed changes
